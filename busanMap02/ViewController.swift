d7bda2f83ade58c3df1bc48ab0e8f88b0a16dc37
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-10-30 13:56:04 +0900

    busanMap 스토리보드설정 , 맵킷추가 , 뷰컨트롤러 추가

--- busanMap02/ViewController.swift
+++ busanMap02/ViewController.swift
@@ -7,18 +7,27 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myMapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "부산 미세먼지 지도"
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let location = CLLocationCoordinate2D(latitude: 35.180100, longitude: 129.081017)
+        // 1.center 지정
+        let span = MKCoordinateSpan(latitudeDelta: 0.27, longitudeDelta: 0.27)
+        // 2.span 지정
+        let region = MKCoordinateRegion(center: location, span: span)
+        // 3.region 지정
+        myMapView.setRegion(region, animated: true)
+        // 4.맵에 보여주기
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
 }
